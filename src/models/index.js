3a93034e726252551d47c7d477d5d9d3c3cd6a25
Your Name <you@example.com>
2021-10-17 20:53:14 +0700

    del

--- src/models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.Token = require('./token.model');
-module.exports.User = require('./user.model');
-module.exports.Lop = require('./lop.model');
-module.exports.Student = require('./student.model');
--- src/validations/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.authValidation = require('./auth.validation');
-module.exports.userValidation = require('./user.validation');
-module.exports.lopValidation = require('./lop.validation');
-module.exports.svValidation = require('./sinhvien.validation');
