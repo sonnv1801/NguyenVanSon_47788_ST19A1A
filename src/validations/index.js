cc7c54d27c9a0976ebcec80819bafcb84df9781e
Your Name <70565643+sonnv1801@users.noreply.github.com>
2021-10-28 12:24:07 +0700

    del

--- src/validations/index.js
+++
@@ -1,7 +0,0 @@
-module.exports.authValidation = require('./auth.validation');
-module.exports.userValidation = require('./user.validation');
-module.exports.productcategoriesValidation = require('./productcategories.validation');
-module.exports.productValidation = require('./product.validation');
-module.exports.sizeValidation = require('./size.validation');
-module.exports.studentValidation = require('./student.validation');
-module.exports.lopValidation = require('./lop.validation');
